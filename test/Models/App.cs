2c04d21195e106fc1ae8d820dcea40028c5c2b3c
EnZDarkSide <vladyavorsky478@gmail.com>
2020-01-12 02:00:28 +0600

    Добавьте файлы проекта.

---
+++ test/Models/App.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+using DevExpress.Mvvm;
+
+namespace WPF_TEST.Models
+{
+    class App : INotifyPropertyChanged
+    {
+        public string Name { get; private set; }
+
+        public string Elapsed { get; private set; }
+
+        public DispatcherTimer timer { get; private set; }
+        private DateTime start = DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public App(string name)
+        {
+            Name = name;
+            timer = InitTimer();
+
+        }
+
+        private DispatcherTimer InitTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += t_Tick;
+            timer.Start();
+            return timer;
+        }
+
+        // Обновление строки таймера
+        private void t_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elpsd = DateTime.Now - start;
+            Elapsed = Convert.ToString(string.Format("{0:00}:{1:00}:{2:00}",
+                elpsd.TotalHours, elpsd.TotalMinutes, elpsd.TotalSeconds));
+
+            // Говорим View, что Elapsed изменился.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Elapsed"));
+        }
+    }
+}
---
+++ test/ViewModels/ProgramViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm;
+using System.Windows.Input;
+using WPF_TEST.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WPF_TEST.ViewModels
+{
+    class ProgramViewModel : ViewModelBase
+    {
+        // Объект приложений
+        public ObservableCollection<App> AppList { get; set; }
+
+        public App SelectedApp { get; set; }
+
+        // Проверка новых приложений
+        public ICommand ClickAdd { 
+            get
+            {
+                return new DelegateCommand(() => {
+                    AppList.Add(new App("Safari"));
+                });
+            }
+        }
+
+        public ProgramViewModel()
+        {
+            AppList = new ObservableCollection<App>
+            {
+                new App("Opera"),
+                new App("Chrome"),
+            };
+        }
+    }
+}
