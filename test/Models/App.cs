63e48f1f8e45c79a65a03d8478b53f52968fddbe
EnZDarkSide <vladyavorsky478@gmail.com>
2020-01-21 12:51:42 +0600

    +график для загрузки процессора приложением +список приложений готов на 65% *Рефакторинг

--- test/Models/App.cs
+++ test/Models/App.cs
@@ -1,25 +1,42 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Threading;
-using DevExpress.Mvvm;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+using LiveCharts;
+using System.Diagnostics;
 
 namespace WPF_TEST.Models
 {
     class App : INotifyPropertyChanged
     {
-        public string Name { get; private set; }
-
-        public string Elapsed { get; private set; }
-
-        public DispatcherTimer timer { get; private set; }
-        private DateTime start = DateTime.Now;
-
         public event PropertyChangedEventHandler PropertyChanged;
+        private DateTime start = DateTime.Now;
+        public DispatcherTimer timer;
 
-        public App(string name)
+        // Тестовая генерация значений для загрузки процессора приложением
+        Random rand = new Random();
+
+        public int Id => Process.Id;
+        public string Name => Process.ProcessName;
+        public string Elapsed { get; set; }
+        public Process Process { get; set; }
+
+        public SeriesCollection ProcessorLoading { get; private set; }
+
+        public App(Process process)
         {
-            Name = name;
+            Process = process;
+
             timer = InitTimer();
+
+            ProcessorLoading = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<LiveCharts.Defaults.ObservableValue>()
+                },
+            };
 
         }
 
@@ -35,12 +52,28 @@ namespace WPF_TEST.Models
         // Обновление строки таймера
         private void t_Tick(object sender, EventArgs e)
         {
-            TimeSpan elpsd = DateTime.Now - start;
-            Elapsed = Convert.ToString(string.Format("{0:00}:{1:00}:{2:00}",
-                elpsd.TotalHours, elpsd.TotalMinutes, elpsd.TotalSeconds));
+            Elapsed = UpdateElapsed();
+            UpdateChartSeries();
+        }
 
-            // Говорим View, что Elapsed изменился.
+        private void UpdateChartSeries()
+        {
+            if (ProcessorLoading[0].Values.Count >= 10)
+                ProcessorLoading[0].Values.RemoveAt(0);
+
+            // TODO: Сделать получение информации о загрузке процессора приложением.
+            //https://stackoverflow.com/questions/9259772/getting-cpu-usage-of-a-process-in-c-sharp <- TODO: исправить на это
+            ProcessorLoading[0].Values.Add(new ObservableValue(rand.Next(0, 100)));
+        }
+
+        private string UpdateElapsed()
+        {
+            TimeSpan elpsd = DateTime.Now - start;
+            var elapsedString = Convert.ToString(string.Format("{0:00}:{1:00}:{2:00}",
+                elpsd.TotalHours, elpsd.TotalMinutes, elpsd.TotalSeconds));
+            //Оповещаем подписчиков через событие PropertyChanged. Знак вопроса заменяет конструкцию if(PropertyChanged != null)
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Elapsed"));
+            return elapsedString;
         }
     }
 }
---
+++ test/ViewModels/AppViewModel.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.Mvvm;
+using System.Windows.Input;
+using WPF_TEST.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+using System.Windows.Threading;
+using System.Linq;
+using System.Diagnostics;
+
+namespace WPF_TEST.ViewModels
+{
+    class AppViewModel : ViewModelBase
+    {
+        // Объект приложений
+        public ObservableCollection<App> AppList { get; set; } = new ObservableCollection<App>();
+        public App SelectedApp { get; set; }
+
+        public AppViewModel()
+        {
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            timer.Tick += UpdateProcesses;
+            timer.Start();
+        }
+
+        private void UpdateProcesses(object sender, EventArgs e)
+        {
+            var processIDs = AppList.Select(p => p.Id).ToList();
+            var newProcesses = Process.GetProcesses();
+
+            foreach (var p in newProcesses)
+            {
+                if (!processIDs.Contains(p.Id)) // появился новый процесс
+                {
+                    AppList.Add(new App(p));
+                }
+            }
+
+            var newProcIds = newProcesses.Select(p => p.Id).ToList();
+            foreach (var p in processIDs)
+            {
+                if(!newProcIds.Contains(p))
+                {
+                    AppList.Remove(AppList.First(proc => proc.Id == p));
+                }
+            }
+        }
+    }
+}
